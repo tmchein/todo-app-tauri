60809f32fe7558f83565b5331bb73960ffc3acbb
Juan Rojas <davidr2998@gmail.com>
2025-11-02 20:27:02 -0500

    chore: adding project configurations

---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: `
+  ▗▄▄▄▖▗▄▄▖ ▗▄▄▖  ▗▄▖ ▗▄▄▖ 
+  ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌
+  ▐▛▀▀▘▐▛▀▚▖▐▛▀▚▖▐▌ ▐▌▐▛▀▚▖
+  ▐▙▄▄▖▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘▐▌ ▐▌
+
+  --------------------------
+  Please use the commit convention
+  --------------------------
+  Or use
+  pnpm commit
+  --------------------------
+  `,
+};
